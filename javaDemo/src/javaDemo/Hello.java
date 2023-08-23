ab6d93e67657f73ba0e011b319bad0c1103a6c84
Admin <Admin@DESKTOP-KJG4M6B>
2023-08-23 20:20:21 +0530

    ccc

---
+++ javaDemo/src/javaDemo/Hello.java
@@ -0,0 +1,20 @@
+package javaDemo;
+
+public class Hello {
+	{
+	int a=2;
+	int b=5;
+	public void add()
+	{
+	int res = a+b;
+	System.out.println(res);
+	}
+	public static void main(String[] args)
+	{
+	Hello c =new Hello();
+	c.add();
+
+	}
+	}
+
+}
